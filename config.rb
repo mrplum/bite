1b254fa807fc218b8d9744851031759934238b0b
Mr. Plum <plumlabs.dev@gmail.com>
2014-02-28 11:41:45 -0300

    Clean up code

--- config.rb
+++ config.rb
@@ -1,13 +1,9 @@
 require 'cordova'
 
 after_configuration do
-  puts "================================"
-  puts "#{root}"
   sprockets.append_path File.join("#{root}", "/bower_components/")
   sprockets.append_path File.join("#{root}", "/bower_components/jquery")
   sprockets.append_path File.join("#{root}", "/bower_components/jquery-mobile")
-  sprockets.append_path File.join("#{root}", "/bower_components/jquery-mobile-bower")
-  puts "================================"
 end
 
 activate :cordova
